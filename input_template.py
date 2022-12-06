e6e48c5c15fa8ee00d15d79f6f6b7518765b2f76
LoganC <alclary@posteo.net>
2022-12-05 20:37:10 -0600

    template for input based problems

---
+++ input_template.py
@@ -0,0 +1,10 @@
+def func(file_name):
+
+    with open(file_name, 'r') as infile:
+        for line in infile:
+            pass
+
+    return
+
+if __name__ == '__main__':
+  func('input\dX_input.txt')
